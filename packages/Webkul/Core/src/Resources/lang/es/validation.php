ba6356f4a8508a1c736d1d6ec517193cc186c53f
ghena <ghenaweb@gmail.com>
2021-11-06 18:22:48 +0100

    Delete packages/Webkul directory

--- packages/Webkul/Core/src/Resources/lang/es/validation.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'slug' => 'O :attribute debe tener un slug válido.',
-    'code' => 'O :attribute debe ser válido.',
-    'decimal' => 'O :attribute debe ser válido.'
-];
--- packages/Webkul/Core/src/Resources/lang/tr/validation.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'slug' => ':attribute değeri geçerli bir url olmalı.',
-    'code' => ':attribute değeri geçerli olmalı.',
-    'decimal' => ':attribute geçerli olmalı.'
-];
